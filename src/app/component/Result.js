a1b47587b66d354f2de3d22be1c3fcdfea054626
Haileab <womahaileab1428@gmail.com>
2024-04-09 18:25:32 +0300

    push this changes

---
+++ src/app/component/Result.js
@@ -0,0 +1,15 @@
+const Results = ({ results })=>{
+    return (
+        <div>
+            {
+                results.map((result)=> {
+                    return (
+                        <div key={result._id}>
+                            <h1>{result.original_title}</h1>
+                        </div>
+                    )})}
+        </div>
+    )
+
+}
+export default Results;
